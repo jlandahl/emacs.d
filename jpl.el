8b664f95fcb7a54d414247b70322ed1c5a777738
John Landahl <jpl@zinfandel.(none)>
2009-08-21 19:34:50 -0700

    added more customizations to jpl.el

--- jpl.el
+++ jpl.el
@@ -1,3 +1,7 @@
+(defmacro with-library (feature &rest body)
+  (when (require ,feature nil t)
+    ,@body)  )
+
 ;;; load-path for misc .el files
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
@@ -16,15 +20,25 @@
 
 ;;; org-mode
 
+(add-to-list 'load-path "~/.emacs.d/vendor/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/vendor/org-mode/contrib/lisp")
+
+(require 'org-install)
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (add-to-list 'load-path "~/.emacs.d/vendor/org-mode")
 (add-hook 'org-mode-hook
-	  (lambda ()
-	    ;; yasnippet
-	    (make-variable-buffer-local 'yas/trigger-key)
-	    (setq yas/trigger-key [tab])
-	    (define-key yas/keymap [tab] 'yas/next-field-group)
-	    ;; flyspell mode to spell check everywhere
-	    (flyspell-mode 1)))
+          (lambda ()
+            ;; yasnippet
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (define-key yas/keymap [tab] 'yas/next-field-group)
+            ;; flyspell mode to spell check everywhere
+            (flyspell-mode 1)))
 
 (org-remember-insinuate)
 (global-set-key (kbd "M-r") 'org-remember)
@@ -46,12 +60,23 @@
 
 (setq message-mode-hook
       (quote (orgstruct++-mode
-	      (lambda nil (setq fill-column 72) (flyspell-mode 1))
-	      turn-on-auto-fill
-	      bbdb-define-all-aliases)))
+              (lambda nil (setq fill-column 72) (flyspell-mode 1))
+              turn-on-auto-fill
+              bbdb-define-all-aliases)))
 
 ;;; groovy-mode
 
 (autoload 'groovy-mode "groovy-mode" "Groovy editing mode." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("groovy" . groovy-mode))
+
+;;; initialize session.el, for saving session state between runs of
+;;; Emacs
+
+(add-hook 'after-init-hook 'session-initialize)
+
+;;; setup epa-file
+
+(when (require 'epa-file nil t)
+  (epa-file-enable)
+  (setq epa-file-cache-passphrase-for-symmetric-encryption t))
