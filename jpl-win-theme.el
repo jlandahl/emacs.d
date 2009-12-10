d5e3e5212d2a03a626cd11da7aa87c5c3d86568a
John Landahl <jpl@zinfandel.(none)>
2009-12-10 14:37:25 -0800

    snapshot

--- jpl-win-theme.el
+++ jpl-win-theme.el
@@ -3,6 +3,6 @@
 
 (custom-theme-set-faces
  'jpl-win
- '(default ((t (:family "Lucida Console" :foundry "unknown" :width normal :height 120 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#141312" :background "#ffffff" :stipple nil :inherit nil)))))
+ '(default ((t (:family "Lucida Console" :foundry "unknown" :width normal :height 110 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#141312" :background "#ffffff" :stipple nil :inherit nil)))))
 
 (provide-theme 'jpl-win)
