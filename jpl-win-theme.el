e64c68506ec5c832df71c88cc9f9f086e573354a
jpl <jpl@spacetime.(none)>
2010-02-08 10:26:24 -0800

    snapshot

--- jpl-win-theme.el
+++ jpl-win-theme.el
@@ -1,8 +1,13 @@
 (deftheme jpl-win
   "Created 2009-11-24.")
 
+(custom-theme-set-variables
+ 'jpl-win
+ '(ispell-program-name "aspell"))
+
 (custom-theme-set-faces
  'jpl-win
- '(default ((t (:family "Lucida Console" :foundry "unknown" :width normal :height 110 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#141312" :background "#ffffff" :stipple nil :inherit nil)))))
+ '(default ((t
+  (:family "DejaVu Sans Mono" :foundry "outline" :width normal :height 98 :weight normal :slant normal :underline nil :overline nil :strike-through nil :box nil :inverse-video nil :foreground "#141312" :background "#ffffff" :stipple nil :inherit nil)))))
 
 (provide-theme 'jpl-win)
